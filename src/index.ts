2c6d89b5a36638bf509bed9951636a7c6b097cee
Jasper <jasper.de.bie1998@gmail.com>
2022-02-03 15:58:38 +0100

    instantiate wokcommands for commands

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import DiscordJS, { Intents } from "discord.js";
 import dotenv from "dotenv";
+import WOKCommands from "wokcommands";
+import path from "path";
 dotenv.config();
 
 const client = new DiscordJS.Client({
@@ -12,6 +14,12 @@ const client = new DiscordJS.Client({
 
 client.on("ready", () => {
 	console.log("Bot is ready!");
+
+	new WOKCommands(client, {
+		commandsDir: path.join(__dirname, "commands"),
+		typeScript: true,
+		testServers: ["938213449161388122"],
+	});
 });
 
 client.login(process.env.TOKEN);
