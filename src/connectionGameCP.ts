632d01ea6d38cb728d56246a5dcbae868a70d78b
Jasper <jasper.de.bie1998@gmail.com>
2022-02-14 15:45:08 +0100

    scrape gamecp for hours

---
+++ src/connectionGameCP.ts
@@ -0,0 +1,26 @@
+const superagent = require("superagent").agent();
+
+const steam = "76561198076890200";
+const massiveLink =
+	`https://grandtheftarma.com/gamecp/api/tools/activity?draw=2&columns%5B0%5D%5Bdata%5D=player_id&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=function&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=function&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=function&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=start&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=function&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=function&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=30&search%5Bvalue%5D=${steam}`;
+
+const ytm = async () => {
+	let dashboard = await superagent
+		.post("https://grandtheftarma.com/gamecp/login/auth")
+		.send({
+			username: "PiratePie",
+			password: "P2ypDqznyYbVPbcGAH2HxxMJMJWhqm236w4dfdth",
+			otp: "641133",
+			remember: "on",
+			redirect: "",
+		})
+		.set("Content-Type", "application/x-www-form-urlencoded");
+
+	const hours = await superagent.get(massiveLink);
+
+	const allData = JSON.parse(hours.text);
+	console.log(allData);
+	console.log(allData.total);
+};
+
+ytm();
