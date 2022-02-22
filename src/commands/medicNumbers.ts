7771333b1f352f3678ec42e9ebae8b080d80076e
Jasper <jasper.de.bie1998@gmail.com>
2022-02-22 01:06:10 +0100

    check if login is successful

--- src/commands/medicNumbers.ts
+++ src/commands/medicNumbers.ts
@@ -1,10 +1,11 @@
-import { MessageEmbed } from "discord.js";
+import { channelMention } from "@discordjs/builders";
+import { Interaction, MessageEmbed } from "discord.js";
 import { ICommand } from "wokcommands";
 import { updateHours } from "../connectionFirebase";
 import { connectToGameCP, getMedics } from "../connectionGameCP";
 
 export default {
-	category: "testing",
+	category: "Management",
 	description: "Return how many people have medic whitelisting",
 	slash: true,
 	testOnly: true,
@@ -12,21 +13,43 @@ export default {
 	minArgs: 3,
 	maxArgs: 3,
 
-	callback: async ({ args }) => {
+	callback: async ({ interaction, args, channel }) => {
 		const username = args[0];
 		const password = args[1];
 		const token = args[2];
 
-		await connectToGameCP(username, password, token);
-		const allMedics = await getMedics();
+		if (await connectToGameCP(username, password, token)) {
+			const embed = new MessageEmbed()
+				.setTitle("Login failed")
+				.setDescription("Login failed lol try again");
+			await interaction.reply({ embeds: [embed], ephemeral: true });
+			return null;
+		}
+		const allMedics: {
+			uid: string;
+			name: string;
+			last_logout: string;
+			join_date: string;
+			permission: string;
+			admin_level: string;
+			permission_level: number;
+			rank: string;
+		}[] = await getMedics();
 
 		const embed = new MessageEmbed()
 			.setTitle("Medic numbers")
 			.setDescription(
-				`There are currently ${allMedics.length} whitelisted medics`
+				`There are currently ${allMedics.length} whitelisted medics.`
 			)
 			.setColor("GREEN");
+		await await interaction.reply({ embeds: [embed], ephemeral: true });
 
-		return embed;
+		channel.send(
+			//todo:make this nice embed
+			`${interaction.user} just ran the xxx command, this command is on a 1 hour cooldown.`
+		);
+
+		// updateHours("Medics", allMedics);
+		return null;
 	},
 } as ICommand;
